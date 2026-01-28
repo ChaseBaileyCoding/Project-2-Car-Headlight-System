470e1ecb7b6d7c7257cb468be5584e643c4989f3
ChaseBailey <baileyc@union.edu>
2026-01-28 14:32:59 -0500

    added comments and made minor fixes

--- main/main.c
+++ main/main.c
@@ -11,6 +11,8 @@
 #define BLUELED_GPIO GPIO_NUM_5
 #define BUZZER_GPIO GPIO_NUM_6
 
+
+
 void app_main(void)
 {
     gpio_reset_pin(DRIVER_GPIO);
@@ -42,11 +44,11 @@ void app_main(void)
     gpio_reset_pin(BUZZER_GPIO);
     gpio_set_direction(BUZZER_GPIO, GPIO_MODE_OUTPUT);
 
-    bool carOn = 0;
-    bool ignitionPressed = 0;
+    bool carOn = 0; //boolean for not looping the message
+    bool ignitionPressed = 0; //boolean to not turn off the vehicle immediately
     printf("\n");
     while(1) {
-        if(gpio_get_level(IGNITION_GPIO)){
+        if(gpio_get_level(IGNITION_GPIO) && ignitionPressed){
             ignitionPressed = 1;
             if (gpio_get_level(DRIVER_GPIO) && gpio_get_level(PASSENGER_GPIO) && gpio_get_level(PASSENGER_SEATBELT_GPIO) && gpio_get_level(DRIVER_SEATBELT_GPIO)){
                 while(1){
@@ -56,12 +58,14 @@ void app_main(void)
                     if(!gpio_get_level(IGNITION_GPIO)){
                         ignitionPressed = 0;
                     }
-                    if(!ignitionPressed && gpio_get_level(IGNITION_GPIO)){
+                    if(!ignitionPressed && gpio_get_level(IGNITION_GPIO)){ //turns off the engine from when it was on
+                        gpio_set_level(BLUELED_GPIO, 0);
+                        ignitionPressed = 1; //sets the value so it does not immediately loop
                         break;
                     }
                 }
             }
-            else{
+            else{ //if the ignition was inhibited enters this loop
                 printf("Ignition inhibited \n");
                 if(!gpio_get_level(DRIVER_GPIO)){
                     printf("Driver seat not occupied \n");
@@ -77,30 +81,32 @@ void app_main(void)
                 }
                 while(1){
                     gpio_set_level(BUZZER_GPIO, 1);
-                    vTaskDelay(25/  portTICK_PERIOD_MS);
+                    vTaskDelay(25/  portTICK_PERIOD_MS); //delays so it is not always testing to fix debounce
                     if(!gpio_get_level(IGNITION_GPIO)){
                         ignitionPressed = 0;
                     }
-                    if(!ignitionPressed && gpio_get_level(IGNITION_GPIO)){
+                    if(!ignitionPressed && gpio_get_level(IGNITION_GPIO)){ //turns off the engine from when it had error
+                        gpio_set_level(BUZZER_GPIO, 0);
+                        ignitionPressed = 1; //sets the value so it does not immediately loop
                         break;
                     }
                 }
             }
         }
-        if(gpio_get_level(DRIVER_GPIO)){
+        if(gpio_get_level(DRIVER_GPIO)){ //checks if the user enters the vehicle
             if(!carOn){
                 printf("Welcome to enhanced alarm system model 218-W25 \n");
                 carOn = 1;
             }
         }
-        else{
+        else{ //statement to make it so the welcome text only plays once each time the button is pressed
             carOn = 0;
         }
         if((gpio_get_level(DRIVER_GPIO)) && (gpio_get_level(PASSENGER_GPIO)) && (gpio_get_level(PASSENGER_SEATBELT_GPIO)) && (gpio_get_level(DRIVER_SEATBELT_GPIO))){
-            gpio_set_level(GREENLED_GPIO, 1);
+            gpio_set_level(GREENLED_GPIO, 1); //turns on the green LED if all of the correct buttons are pressed
         }
         else{
-            gpio_set_level(GREENLED_GPIO, 0);
+            gpio_set_level(GREENLED_GPIO, 0); //turns it off after the loop
         }
         vTaskDelay(25/  portTICK_PERIOD_MS);
     }
